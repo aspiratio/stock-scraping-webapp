1af9f043e31baf19ecf6a00ea9eed2496bf0525a
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-10-16 22:44:00 +0900

    ヘッドレスモードでスクレイピングするように変更

--- backend/stock_scraping_service/main.py
+++ backend/stock_scraping_service/main.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
@@ -141,8 +142,13 @@ def get_own_stock_df(driver):
 
 
 if __name__ == "__main__":
+    # Chrome オプションの設定
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # ヘッドレスモードを有効にする
     # ドライバーの起動
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=chrome_options
+    )
 
     try:
         # 証券会社のwebサイトから保有株情報を抽出する
