ef74157e5748fcbaea84a2d0d483a2f4134caeac
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-10-22 17:27:04 +0900

    json形式で動作確認

--- backend/stock_scraping_service/firestore_utils.py
+++ backend/stock_scraping_service/firestore_utils.py
@@ -1,3 +1,4 @@
+import json
 from google.cloud import firestore
 import config
 
@@ -6,26 +7,36 @@ root_collection_name = config.FIRESTORE_COLLECTION
 # 最上流配下のドキュメントID（環境毎にDBを変える場合はここで分ける）
 root_doc_id = config.FIRESTORE_DOCUMENT
 
+# Firestoreに接続
+db = firestore.Client()
+root_doc = db.collection(root_collection_name).document(root_doc_id)
 
-def get_document_ids(collection_name):
+
+def get_document_ids(collection_name: str):
     try:
-        # Firestoreに接続
-        db = firestore.Client()
-
-        # コレクションを取得（サブコレクションの中に目的のコレクションがある）
-        collection = (
-            db.collection(root_collection_name)
-            .document(root_doc_id)
-            .collection(collection_name)
-        )
+        # コレクションを取得
+        collection = root_doc.collection(collection_name)
 
         # すべてのドキュメントを取得
         docs = collection.get()
 
         # ドキュメントIDのみを抽出
-        docs_ids = [doc.id for doc in docs]
+        doc_ids = [doc.id for doc in docs]
 
-        return docs_ids
+        return doc_ids
+
+    except Exception as e:
+        print(str(e))
+
+
+def set_documents(collection_name: str, json_values: json):
+    values = json.loads(json_values)
+    try:
+        for value in values:
+            print(value)
+            # ドキュメントを指定
+            # doc_ref = root_doc.collection(collection_name).document(value.ticker)
+            # doc_
 
     except Exception as e:
         print(str(e))
