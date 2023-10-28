8710e9c8663e63fb44dde183a87ddd9a74355baf
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-10-29 00:45:09 +0900

    Cloud Run の環境変数を受け取れるように変更

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -1,15 +1,32 @@
-# TODO: Cloud Run上で動すときは Secret Manager から認証情報を持ってくるようにする
-# .env ファイルをロードして環境変数へ反映
+# 環境変数を取得する
+import os
+import json
 from dotenv import load_dotenv, find_dotenv
 
+# ローカルの .env ファイルをロードする
 load_dotenv(find_dotenv())
 
-# 環境変数を参照
-import os
+# Cloud Run の環境変数を取得する
+# ローカルには ACCOUNT_INFO の環境変数はないため、None になる。その時だけローカルの環境変数を利用する
+secret_key_json = os.getenv("ACCOUNT_INFO")
+sbi_username = ""
+sbi_password = ""
+spreadsheet_key = ""
 
-SBI_USERNAME = os.getenv("SBI_USERNAME")
-SBI_PASSWORD = os.getenv("SBI_PASSWORD")
-SPREADSHEET_KEY = os.getenv("SPREADSHEET_KEY")
+if secret_key_json is None:
+    sbi_username = os.getenv("SBI_USERNAME")
+    sbi_password = os.getenv("SBI_PASSWORD")
+    spreadsheet_key = os.getenv("SPREADSHEET_KEY")
+else:
+    sbi_username = json.loads(secret_key_json)["SBI_USERNAME"]
+    sbi_password = json.loads(secret_key_json)["SBI_PASSWORD"]
+    sbi_password = json.loads(secret_key_json)["SPREADSHEET_KEY"]
+
+
+SBI_USERNAME = sbi_username
+SBI_PASSWORD = sbi_password
+SPREADSHEET_KEY = spreadsheet_key
+
 SHEET_NAME = os.getenv("SHEET_NAME")
 FIRESTORE_COLLECTION = os.getenv("FIRESTORE_COLLECTION")
 FIRESTORE_DOCUMENT = os.getenv("FIRESTORE_DOCUMENT")
