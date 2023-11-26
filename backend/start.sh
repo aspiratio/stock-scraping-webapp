0b5f483d8352c14df744d0dfa44d67cf1cc34b7a
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-11-26 16:21:05 +0900

    パスの修正

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$ENVIRONMENT" = "local" ]; then
-  cd /app/app && uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+  cd /app && uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 else
-  cd /app/app && uvicorn main:app
+  cd /app && uvicorn main:app
 fi
