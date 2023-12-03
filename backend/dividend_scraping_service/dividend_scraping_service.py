f1711ed2b0ae4c1b3f0e5ab913ed369936e2bbe6
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-12-03 21:47:58 +0900

    CloudRunデバッグ用

--- backend/dividend_scraping_service/dividend_scraping_service.py
+++ backend/dividend_scraping_service/dividend_scraping_service.py
@@ -55,18 +55,18 @@ async def dividend_scraping():
 
         print(stock_codes)
 
-        stock_code, dividend = _process_stock(stock_codes[0])
-        results.append({"ticker": stock_code, "dividend": dividend})
+        # stock_code, dividend = _process_stock(stock_codes[0])
+        # results.append({"ticker": stock_code, "dividend": dividend})
 
-        # with concurrent.futures.ProcessPoolExecutor(max_workers=5) as executor:
-        #     futures = [
-        #         executor.submit(_process_stock, stock_code)
-        #         for stock_code in stock_codes
-        #     ]
+        with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
+            futures = [
+                executor.submit(_process_stock, stock_code)
+                for stock_code in stock_codes
+            ]
 
-        #     for future in concurrent.futures.as_completed(futures):
-        #         stock_code, dividend = future.result()
-        #         results.append({"ticker": stock_code, "dividend_yen": dividend})
+            for future in concurrent.futures.as_completed(futures):
+                stock_code, dividend = future.result()
+                results.append({"ticker": stock_code, "dividend_yen": dividend})
 
         # 結果をDBに登録する
         set_documents(output_collection_name, results)
